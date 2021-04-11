d2d89508fd292b4893624f49320c366b863d2b1a
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-11 17:32:54 +0530

    ADDED : welcome message to the program

---
+++ src/main/java/com/simpleLinkedList/MyNodeTest.java
@@ -0,0 +1,6 @@
+package com.simpleLinkedList;
+public class MyNodeTest<T> {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Simple Linked List program");
+    }
+}
---
+++ src/test/java/com/simpleLinkedList/LinkedListsTest.java
@@ -0,0 +1,9 @@
+package com.simpleLinkedList;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class LinkedListsTest {
+
+    }
+
