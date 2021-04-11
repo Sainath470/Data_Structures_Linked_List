21618609488b329bfe2c8024a032fab35659428b
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-11 17:35:52 +0530

    ADDED : created simple linked list

--- src/main/java/com/simpleLinkedList/MyNodeTest.java
+++ src/main/java/com/simpleLinkedList/MyNodeTest.java
@@ -1,6 +1,19 @@
 package com.simpleLinkedList;
 public class MyNodeTest<T> {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Simple Linked List program");
-    }
+        private T key;
+        private MyNodeTest next;
+
+    public MyNodeTest(T key) {
+            this.key = null;
+            this.next = null;
+        }
+
+        public MyNodeTest getNext () {
+            return next;
+        }
+
+        public void setNext (MyNodeTest next){
+            this.next = next;
+        }
 }
+
--- src/test/java/com/simpleLinkedList/LinkedListsTest.java
+++ src/test/java/com/simpleLinkedList/LinkedListsTest.java
@@ -4,6 +4,18 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class LinkedListsTest {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest() {
+        MyNodeTest<Integer> myFirstNode = new MyNodeTest<>(56);
+        MyNodeTest<Integer> mySecondNode = new MyNodeTest<>(30);
+        MyNodeTest<Integer> myThirdNode = new MyNodeTest<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
 
     }
+
+}
 
